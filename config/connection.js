6e429ead2f54157d0bef17f2f802ac26a2aae234
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-15 16:00:19 -0400

    reworked jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 require("dotenv").config();
-
+let connection;
 //using .env
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+  });
+}
 // Make connection.
 connection.connect(function (err) {
   if (err) {
