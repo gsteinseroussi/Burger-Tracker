1f555707723facf6fcf89251a654e7cb290f6012
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-15 15:31:02 -0400

    using jaws

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "burgers_db",
+  database: process.env.DB_DATABASE,
 });
 
 // Make connection.
